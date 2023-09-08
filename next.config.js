091cf1f34e58dc9223dbfc6d1a800f7d5cf52b30
prazzdev <prazzid31@gmail.com>
2023-09-09 00:02:13 +0700

    modified:   next.config.js 	modified:   src/pages/index.js 	modified:   src/styles/globals.css

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.bakul.buzz',
+        port: '',
+        pathname: '/data/**',
+      },
+    ],
+  },
+}
